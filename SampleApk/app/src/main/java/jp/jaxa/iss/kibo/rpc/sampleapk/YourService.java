335fb55065bad4a103d13e6f20e521e87be029c4
Mauricio Nava <mauguicho.nava@gmail.com>
2025-05-28 21:28:20 -0500

    Path has been created

--- SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -40,23 +40,31 @@ public class YourService extends KiboRpcService {
         Mat image = api.getMatNavCam();
         api.saveMatImage(image, "Area1");
 
+        point = new Point(10.42d, -10.58d, 4.82d);
+        quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
         /* ******************************************************************************** */
-        /* Write your code to recognize the type and number of landmark items in each area! */
+        /* Write your code  to recognize the type and number of landmark items in each area! */
         /* If there is a treasure item, remember it.                                        */
         /* ******************************************************************************** */
-
+        point= new Point(10.3d, -9.254d, 3.76203d);
+        quaternion= new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
         // When you recognize landmark items, let’s set the type and number.
 //        api.setAreaInfo(1, "item_name", 1);
-
+        point = new Point(10.3d, -8.4d, 3.76203d);
+        quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
         /* **************************************************** */
         /* Let's move to each area and recognize the items. */
         /* **************************************************** */
 
         // When you move to the front of the astronaut, report the rounding completion.
-//        point = new Point(11.143d, -6.7607d, 4.9654d);
-//        quaternion = new Quaternion(0f, 0f, 0.707f, 0.707f);
-//        api.moveTo(point, quaternion, false);
-//        api.reportRoundingCompletion();
+        point = new Point(11.143d, -6.7607d, 4.9654d);
+        quaternion = new Quaternion(0f, 0f, 0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
+
+
 
 
         /* ********************************************************** */
